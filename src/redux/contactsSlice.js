eae70910d2d57f3627433a298f7429cd474bd960
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-04-07 15:43:15 +0300

    task done with redux-persist

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,19 +2,32 @@ import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from 'nanoid';
 
 const contactsInitialState = [
-	{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-	{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-	{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-	{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+	// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+	// { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+	// { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+	// { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
 
 const contactsSlice = createSlice({
 	name: 'contacts',
 	initialState: contactsInitialState,
 	reducers: {
 		addContact: {
-			reducer(state, action) { 
-				return [...state, action.payload]
+			reducer(state, action) {
+				let isNameRepeat = false;
+				state.map(contact => {
+					if (contact.name.toLowerCase() === action.payload.name.toLowerCase()) {
+						alert(`${contact.name} is already in contacts.`);
+						isNameRepeat = true;
+					}
+					return null;
+				});
+				
+				if (isNameRepeat) {
+					return [...state];
+				} else {
+					return [...state, action.payload];
+				}
 			},
 			prepare(contact) {
 				return {
@@ -33,6 +46,6 @@ const contactsSlice = createSlice({
 	}
 })
 
-export const { addContact, deleteContact } = contactsSlice.actions;
-
 export const contactsReducer = contactsSlice.reducer;
+
+export const { addContact, deleteContact } = contactsSlice.actions;
